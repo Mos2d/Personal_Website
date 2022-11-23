215828e2ae890d7e48997826ece0f9f955c1cf8d
Youssof Mohamed <90064094+Mos2d@users.noreply.github.com>
2022-11-23 08:26:38 -0500

    appearOnScroll

--- app.js
+++ app.js
@@ -50,3 +50,27 @@ let navbarHeight = 0 - document.getElementById("navbar").offsetHeight;
   }
   prevScrollPosition = currScrollPosition;
 }
+
+const faders = document.querySelectorAll('.fade-in');
+
+console.log(faders);
+
+const appearOptions = {
+  threshold: 0.7
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
