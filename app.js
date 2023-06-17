e838b2ef8adf6180db9369ada01393061a028aa1
Youssof Mohamed <90064094+Mos2d@users.noreply.github.com>
2023-06-17 00:50:14 -0400

    refactoring
    changing the whole design of the website (finished intro and about me sections)

--- app.js
+++ app.js
@@ -1,54 +1,56 @@
-let slideIndex = 1;
-showSlides(slideIndex, "mySlides", "dot");
-showSlides(slideIndex, "mySlides2", "dot2");
-showSlides(slideIndex, "mySlides3", "dot3");
-showSlides(slideIndex, "mySlides4", "dot4");
-showSlides(slideIndex, "mySlides5", "dot5");
-showSlides(slideIndex, "mySlides6", "dot6");
-showSlides(slideIndex, "mySlides7", "dot7");
-showSlides(slideIndex, "mySlides8", "dot8");
-showSlides(slideIndex, "mySlides9", "dot9");
-showSlides(slideIndex, "mySlides10", "dot10");
-showSlides(slideIndex, "mySlides11", "dot11");
-showSlides(slideIndex, "mySlides12", "dot12");
+// let slideIndex = 1;
+// showSlides(slideIndex, "mySlides", "dot");
+// showSlides(slideIndex, "mySlides2", "dot2");
+// showSlides(slideIndex, "mySlides3", "dot3");
+// showSlides(slideIndex, "mySlides4", "dot4");
+// showSlides(slideIndex, "mySlides5", "dot5");
+// showSlides(slideIndex, "mySlides6", "dot6");
+// showSlides(slideIndex, "mySlides7", "dot7");
+// showSlides(slideIndex, "mySlides8", "dot8");
+// showSlides(slideIndex, "mySlides9", "dot9");
+// showSlides(slideIndex, "mySlides10", "dot10");
+// showSlides(slideIndex, "mySlides11", "dot11");
+// showSlides(slideIndex, "mySlides12", "dot12");
 
-// Next/previous controls
-function plusSlides(n, slidesClassName, dotClassName) {
-  showSlides(slideIndex += n, slidesClassName, dotClassName);
-}
+// // Next/previous controls
+// function plusSlides(n, slidesClassName, dotClassName) {
+//   showSlides(slideIndex += n, slidesClassName, dotClassName);
+// }
 
-// Thumbnail image controls
-function currentSlide(n, slidesClassName, dotClassName) {
-  showSlides(slideIndex = n, slidesClassName, dotClassName);
-}
+// // Thumbnail image controls
+// function currentSlide(n, slidesClassName, dotClassName) {
+//   showSlides(slideIndex = n, slidesClassName, dotClassName);
+// }
 
-function showSlides(n, slidesClassName, dotClassName) {
-  let i;
-  let slides = document.getElementsByClassName(slidesClassName);
-  let dots = document.getElementsByClassName(dotClassName);
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active-dot", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active-dot";
-}
+// function showSlides(n, slidesClassName, dotClassName) {
+//   let i;
+//   let slides = document.getElementsByClassName(slidesClassName);
+//   let dots = document.getElementsByClassName(dotClassName);
+//   if (n > slides.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = slides.length}
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active-dot", "");
+//   }
+//   slides[slideIndex-1].style.display = "block";
+//   dots[slideIndex-1].className += " active-dot";
+// }
 
 var prevScrollPosition = window.pageYOffset;
 
 window.onscroll = function() {
   var currScrollPosition = window.pageYOffset;
-let navbarHeight = 0 - document.getElementById("navbar").offsetHeight;
+  let navbarHeight = 0 - document.getElementById("navbar").offsetHeight;
   if (currScrollPosition < prevScrollPosition) {
     document.getElementById("navbar").style.top = "0px";
   } else {
     document.getElementById("navbar").style.top = navbarHeight + "px";
   }
   prevScrollPosition = currScrollPosition;
+  var $nav = $(".navbar");
+  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 }
 
 const faders = document.querySelectorAll('.fade-in');
@@ -77,22 +79,22 @@ faders.forEach(fader => {
 
 // pop-up
 
-var modal = document.getElementById("myModal");
+// var modal = document.getElementById("myModal");
 
-var btn = document.getElementById("myBtn");
+// var btn = document.getElementById("myBtn");
 
-var span = document.getElementsByClassName("close")[0];
+// var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
 
-span.onclick = function() {
-  modal.style.display = "none";
-}
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
